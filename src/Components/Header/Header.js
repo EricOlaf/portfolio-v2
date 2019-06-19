fade4301dc381a5a6830f6a3d47167a04d953968
Eric Olaveson <olavesoneric@gmail.com>
2019-06-19 15:05:43 -0500

    arrow

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 
+import { Link } from "react-scroll";
+
 function Header(props) {
   return (
     <header>
@@ -10,6 +12,7 @@ function Header(props) {
         src="https://res.cloudinary.com/rawtrails801/image/upload/v1549427681/RawTrails/IMG_1963.JPG.jpg"
         alt=""
       />
+ 
       <div id="text">
         <h1 className="title">ERIC OLAVESON</h1>
         <p id="headerP">I design, build and maintain websites</p>
@@ -20,6 +23,16 @@ function Header(props) {
         >
           CONTACT ME
         </button>
+        <div className="center">
+        <Link
+            to="skills"
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+          <img  className="arrow" src="https://res.cloudinary.com/rawtrails801/image/upload/v1560972570/arrow-204-48.png" alt=""/>
+          </Link>
+        </div>
       </div>
     </div>
     </header>
